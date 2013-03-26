6d7fe8bbd950862d8589827915923107a4b0563b
Piotr Hajduga <kosarock@gmail.com>
2013-03-26 22:57:48 +0100

    Refactoring and tests cleanup

--- gol.py
+++ gol.py
@@ -4,12 +4,17 @@ from functools import lru_cache
 
 class GameOfLife(object):
     ruleset = {
-        'last': (2, 3),
+        'stay_alive': (2, 3),
         'breed': (3,),
     }
 
-    def __init__(self, state):
-        self.current_state = state
+    generations_count = 0
+
+    def __init__(self, state=[]):
+        self.set_state(state)
+
+    def set_state(self, state):
+        self.current_state = set(state)
 
     def get_neighbour_count(self, x, y):
         count = 0
@@ -20,36 +25,38 @@ class GameOfLife(object):
                 count += 1
         return count
 
+    def is_alive(self, x, y):
+        return (x, y) in self.current_state
+
     @lru_cache(maxsize=8)
     def get_new_state(self, x, y):
         count = self.get_neighbour_count(x, y)
         alive = (x, y) in self.current_state
         return (
-            alive and count in self.ruleset['last']
+            alive and count in self.ruleset['stay_alive']
             or not alive and count in self.ruleset['breed']
         )
 
     def get_neighbours(self, x, y):
-        for point in product(
-            range(x - 1, x + 2),
-            range(y - 1, y + 2)
-        ):
-            if point != (x, y):
-                yield point
+        return (
+            point for point in product(
+                range(x - 1, x + 2),
+                range(y - 1, y + 2)
+            ) if point != (x, y)
+        )
 
     def next_state(self):
         self.get_new_state.cache_clear()
-        result = set()
+        state = set()
         for point in self.current_state:
             if self.get_new_state(*point):
-                result.add(point)
+                state.add(point)
             for neighbour in self.get_neighbours(*point):
-                if neighbour in self.current_state:
-                    continue
                 if self.get_new_state(*neighbour):
-                    result.add(neighbour)
-        self.current_state = result
-        return result
+                    state.add(neighbour)
+        self.current_state = state
+        self.generations_count += 1
+        return state
 
 
 def print_state(gol, start, end):
--- test.py
+++ test.py
@@ -1,25 +1,34 @@
 import unittest
 from gol import GameOfLife
 
+
 class Test(unittest.TestCase):
     def setUp(self):
-        self.gol = GameOfLife(set([(1,1),(1,2),(1,3),(2,3)]))
-    
-    def testNeighbourCount(self):
-        self.assertEquals(1,self.gol.get_neighbour_count(1,1))
+        self.gol = GameOfLife(
+            set([(1, 1), (1, 2), (1, 3), (2, 3)])
+        )
 
-    def testGetNewStateDie(self):
-        assert(self.gol.get_new_state(1,1)==False)
+    def test_neighbour_count(self):
+        self.assertEqual(1, self.gol.get_neighbour_count(1, 1))
 
-    def testGetNewStateStayAlive(self):
-        assert(self.gol.get_new_state(1,2))
+    def test_get_new_state_die(self):
+        self.assertFalse(self.gol.get_new_state(1, 1))
 
-    def testGetNewStateBreed(self):
-        assert(self.gol.get_new_state(0,2))
+    def test_get_new_state_stay_alive(self):
+        self.assertTrue(self.gol.get_new_state(1, 2))
 
-    def testGetNewStateBreed(self):
-        assert(self.gol.get_new_state(2,2)==False)
-    
-    def testNextState(self):
-        expected = set([(0,2),(1,2),(1,3),(2,3)])
-        self.assertEquals(expected,self.gol.next_state())
+    def test_get_new_state_breed(self):
+        self.assertTrue(self.gol.get_new_state(0, 2))
+        self.assertFalse(self.gol.get_new_state(2, 2))
+
+    def test_next_state(self):
+        expected = set([(0, 2), (1, 2), (1, 3), (2, 3)])
+        generations_count = self.gol.generations_count
+        self.assertEqual(expected, self.gol.next_state())
+        self.assertEqual(generations_count + 1, self.gol.generations_count)
+
+    def test_set_state(self):
+        expected = set([(1, 2), (-2, 0), (0, 0)])
+        self.gol.set_state(expected)
+        self.assertEqual(expected, self.gol.current_state)
+        self.assertEqual(0, self.gol.generations_count)
